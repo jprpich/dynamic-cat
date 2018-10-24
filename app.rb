36b2793faa814a8198ce659a19aa19f9b499564f
Joshua <joshuaprpich@gmail.com>
2018-10-24 10:20:29 -0500

    make site dynamic

--- app.rb
+++ app.rb
@@ -4,3 +4,16 @@ get '/' do
   erb :index
 end
 
+# access params["name"] from paramater variables
+get '/cat/:name' do
+  @name = params["name"]
+  erb :cat
+end
+
+# asign params["name"] from the query string
+get '/dog/name' do
+  @name = params["name"]
+  @last_name = params["last_name"]
+  erb :dog
+end
+
